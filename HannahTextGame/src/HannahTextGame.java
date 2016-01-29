7bfbaded5097247c834ce0c5d0af49c267776fe1
Hannah Riedman <hannah.riedman1@marist.edu>
2016-01-29 16:19:24 -0500

    added while loop and ready to start adding commands

--- HannahTextGame/src/HannahTextGame.java
+++ HannahTextGame/src/HannahTextGame.java
@@ -13,16 +13,27 @@ public class HannahTextGame {
 	public static void main(String[] args) {
 		
 		String name;
-		
-		
+		String gender;
+		String direction;
+		String userCommand;
 		
 		System.out.println("Murder Mystery Game!");
 		System.out.println("********************\n");
-		System.out.println("You arrive at the creepy mansion in the dark of night. "
+		System.out.println("You arrive at the creepy mansion in the dark of night.\n"
 							+ "There is a large door in front of you.");
 		System.out.print("What is your character's name?");
 		name = keyboard.nextLine();
+		System.out.print("What is your character's gender?(m or f)");
+		gender = keyboard.nextLine();
 		
+		System.out.print("Hello Detective " + name + "! You can navigate the game by using\n"
+				+ "N,E,S,W commands. Go North to the door to start the adventure!");
+		while (true) {
+			
+			
+
+		
+		}
 
 	}
 
