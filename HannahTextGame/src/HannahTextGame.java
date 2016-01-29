afdfe6a0adb9defe7f7cf1b59ae38d16df1d378e
Hannah Riedman <hannah.riedman1@marist.edu>
2016-01-29 13:36:17 -0500

    added input from keyboard and comment with name of game.

--- HannahTextGame/src/HannahTextGame.java
+++ HannahTextGame/src/HannahTextGame.java
@@ -1,5 +1,14 @@
 
+import java.util.Scanner;
+
+/**
+ * A text Adventure Murder Mystery game.
+ * @author Hannah Riedman
+ *
+ */
 public class HannahTextGame {
+	
+	static Scanner keyboard = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
