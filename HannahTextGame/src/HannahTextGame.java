3e3527cc61da3bbee09bbb4c815a4815d66a8ea3
Hannah Riedman <hannah.riedman1@marist.edu>
2016-02-08 20:27:34 -0500

    Final commit

--- HannahTextGame/src/HannahTextGame.java
+++ HannahTextGame/src/HannahTextGame.java
@@ -13,61 +13,55 @@ import java.util.Scanner;
  */
 public class HannahTextGame {
 	
-	static String [] descriptions={
-			"You arrive at the creepy mansion in the dark of night.\n "
+	static String [] descriptions={  // Array to hold descriptions for locations
+			"You arrive at the creepy mansion in the dark of night.\n"
 			+ "There is a large door in front of you.", // outside 0
 			"You are now in the Foyer. There is a study to the East and sitting room\n"
 			+ "to the West.", // Foyer 1
 			"You are now in the Study. To the North is a door.", // Study 2
 			"You are now in the sitting room.", // sitting room 3
 			"You are now in the Library", // library 4		
-			"You have found a secret stairway after leaning on the bookcase." // secret stairway
+			"You have found a secret stairway after leaning on the bookcase." // secret stairway 5
 	};
 	
-	static int location = 0;
-	
+	static int location = 0; // static variable declarations 
 	static String name;
 	static String gender;
-		
-	// TODO separate out some tasks (getting input, updating variable,
-	//      displaying output) into their own functions and call those
-	//      functions from within this main method
 	
 	static Scanner keyboard = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
-		String direction;
+		String direction;  // variable declarations 
 		String userCommand;
 		
-		System.out.println("Murder Mystery Game!");
+		System.out.println("Murder Mystery Game!");  // intro and character customization
 		System.out.println("********************\n");
-		System.out.println(descriptions[0]);
 		System.out.print("What is your character's name?");
 		name = keyboard.nextLine();
 		System.out.print("What is your character's gender?(m or f)");
 		gender = keyboard.nextLine();
 		
-		System.out.print("Hello Detective " + name + "! You can navigate the game by using\n"
+		System.out.println(descriptions[0]);  // start of game
+		System.out.println("*************************************");
+		System.out.println("Hello Detective " + name + "! You can navigate the game by using\n"
 				+ "N,E,S,W commands. Go North to the door to start the adventure!");
+		
 		while (true) {
 			
 			System.out.print("Enter a command: ");
 			userCommand = keyboard.nextLine();
-			userCommand = userCommand.toUpperCase();
+			userCommand = userCommand.toUpperCase(); // makes input case sensititive.
 	
-			direction = ProcessDirection(userCommand); 
+			direction = ProcessDirection(userCommand); // changes input into directions/commands
 		
 			if (direction.equals("quit")) {  // checks to see if you want to start over or quit
-				break;
+				break;  // quits game
 			} else if (direction.equals("start over")) {
-				continue;
+				continue; // starts loop over 
 			}
 			
-			Navigation(direction);
-			//now "render" the game state by providing feedback to the player
-			// TODO change this so that you print the description of the new location
-			//System.out.println("You moved " + direction + ".\n");
+			Navigation(direction); // takes direction input and changes location   
 
 		
 		}
@@ -107,6 +101,7 @@ public class HannahTextGame {
 	
 	public static int ProcessLocation(int location) {
 
+		// this method takes the location and displays the description 
 		
 		System.out.println(descriptions[location]);
 		return location;
@@ -114,6 +109,8 @@ public class HannahTextGame {
 	}
 		
 	public static void Navigation (String direction) {
+		
+		// this method takes direction and changes location and displays description
 		
 		if (direction.equals("North")) {
 			if (location == 0) {
@@ -129,7 +126,7 @@ public class HannahTextGame {
 			} else if (location == 0 || location == 2 || location == 4){
 				System.out.print("You can not go East.");
 			} else if (location == 3) {
-				System.out.print(descriptions[1]); // back to Foyer 
+				System.out.print(descriptions[1]); 
 				location = 1;
 			} else if (location == 5) {
 				location = ProcessLocation(4);
