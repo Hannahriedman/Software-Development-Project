3d185bddae0bec275897ea979128048f54150490
Hannah Riedman <hannah.riedman1@marist.edu>
2016-04-02 11:16:00 -0400

    finished drop and take

--- HannahTextGame/src/Player.java
+++ HannahTextGame/src/Player.java
@@ -30,15 +30,16 @@ public class Player {
 	public void take(Locale [] loc,String item) {
 		Locale currentloc = loc[this.location];
 		Item   itemHere   = currentloc.item;
+		String itemToTake = item;
 		
 		if (itemHere == null) { // checks if there is item at location 
 			System.out.println("There is no item here to take");
 		} else { 
-			if (itemHere.isDiscoverd) {
+			if (itemHere.isDiscoverd && itemHere.name.equals(itemToTake)) {
 				this.inventory.add(itemHere); 
 				currentloc.item = null; 
-				this.score += 5;
-				System.out.println("You now have a " + itemHere.name + " .\n" 
+				this.score += itemHere.value;
+				System.out.println("You now have a " + itemHere.name + ".\n" 
 				+ itemHere.descrip);
 			} else {
 				System.out.println("There is no visble item to take.");
@@ -48,11 +49,12 @@ public class Player {
 	
 	/**
 	 * Drop method
-	 * Prompts user what they want to drop, Checks if
+	 * intakes what user wants to drop, Checks if
 	 * there is an object already at location, if not 
-	 * then checks if user input equals the item there.
-	 * Then it drops item and subtracts 5 points.
+	 * then checks if user input equals an item in inventory.
+	 * Then it drops item and subtracts its worth.
 	 * @param loc is the location array of loc Objects.
+	 * @param item is the item the user wishes to drop.
 	 */
 	public void drop(Locale [] loc,String item) {
 		Locale currentloc = loc[this.location];
@@ -68,7 +70,7 @@ public class Player {
 					System.out.println("You have dropped the " + itemToDrop);
 					currentloc.item = checkItem;
 					this.inventory.remove(i);
-					this.score -= 5;
+					this.score -= checkItem.value;
 				} 
 				
 			}
