a1cdd2aebc0d3540d606ec70b04a16125783fb65
Hannah Riedman <hannah.riedman1@marist.edu>
2016-04-04 11:04:09 -0400

    added more java doc messages

--- HannahTextGame/src/HannahTextGame.java
+++ HannahTextGame/src/HannahTextGame.java
@@ -33,11 +33,19 @@ public class HannahTextGame {
 	
 	static Scanner keyboard = new Scanner(System.in);
 	
+	/**
+	 * Initalizations of Items
+	 * 3 Items and 1 LimitedUseItem
+	 */
 	static Item map1 =                  new Item("map","This is a map of the mansion.Type M to use it.","Type M to see map.",10);
 	static Item rope =                  new Item("rope","This is a thick braided rope.","The rope does nothing, you put it back.",5);
 	static Item knife =                 new Item("knife","This knife is bloody.","You aren't good with knifes, you cut yourself.",10);
 	static LimitedUseItem candlestick = new LimitedUseItem("candlestick","This is a candlestick.","The room is brighter now.",15,5);
 	
+	/**
+	 * Initalizations of Locations
+	 * 9 Locations
+	 */
 	static Locale outside =        new Locale(
 			"Outside",
 			"You arrive at the creepy mansion in the dark of night.\n"
@@ -79,10 +87,17 @@ public class HannahTextGame {
 			"There is a door to the north.",
 			null);
 	
-	
+	/**
+	 * Location Array to keep all location Locale's
+	 */
 	static Locale [] locations={outside,foyer,study,sittingRoom,library,secretStairway,kitchen,hall,diningRoom};
 								// 0      1      2       3          4          5         6      7      8
-	
+	/**
+	 * Matrix map to navigate the game
+	 * using integers to represent the index of the location in the Location array above
+	 * and the cardnial directions users enter from a specific location.
+	 * -1 represents where there is no way of going that way from that location.
+	 */
 	static int [][] map = {
 	        // NORTH, EAST, SOUTH, WEST 
 	           {1, -1, -1, -1 }, // from outside: Foyer ---
@@ -95,9 +110,14 @@ public class HannahTextGame {
 	           { -1, -1, 1, 8 }, // from hall: --Foyer diningRoom
 	           { 6, 7, -1, -1 } // from dining room: kitchen hall--
 	};
-	
+	/**
+	 * Initaixation of player 
+	 */
 	static Player player1 =    new Player(playerName,location,gender,inventory);
 	
+	/**
+	 * Intialization of BreadcrumbTrail
+	 */
 	static LinkedTrail trail = new LinkedTrail();
 
 	public static void main(String[] args) {
@@ -128,7 +148,11 @@ public class HannahTextGame {
 
 	}
 	
-	// messages methods
+	/**
+	 *  Messages methods
+	 *  Intro Message that starts everygame.
+	 *  GoodBye method that ends game.
+	 */
 	public static void introMessage() {
 		
 		System.out.println("Murder Mystery Game!");  // Title Screen
@@ -167,7 +191,16 @@ public class HannahTextGame {
 		};
 	}
 	
-	// user input method
+	/**
+	 * User Input Method
+	 * This method will intake user input and check to see
+	 * if it matches one of the commands. If so it will properely 
+	 * do whatever the command specifies and return "start over" so the 
+	 * command prompt will appear agian once command is completeed
+	 *  or it will return invalid command and prompt will reappear.
+	 * @param userCommand: String of what the user typed in.
+	 * @return "direction": String of start over or quit to repeat loop or get out of it
+	 */
     public static String processDirection(String userCommand) {
 		String direction = null;
 		
@@ -220,13 +253,42 @@ public class HannahTextGame {
 		return direction;
 	}
 	
-	// navigation methods
+    /** 
+	 * String Converter Method
+	 * This is a Homemade toString method to properly convert 
+	 * user input array after input is split into a string. 
+	 * @param userinput: Array of split user input
+	 * @return userinput into proper string that is lower case and trimmed 
+	 */
+	public static String stringConverter (String[] userinput) {
+		StringBuilder sb = new StringBuilder();
+		
+        for(String str : userinput) sb.append(str);
+        return sb.toString().toLowerCase().trim();
+		
+	}
+    
+    // Navigation Methods: FROM & MOVE
+	/**
+	 * From Method: Takes in the cardnial direction as int and returns locale index
+	 * @param dir the integer of the cardnail direction user wants to go 
+	 * @return what int is at that spot in the map matrix (using current
+	 * location and direction)
+	 */
 	public static int from (int dir) {
 		int currentLocation = player1.location;
 		
 		return map[currentLocation][dir];
 		
 	}
+	/**
+	 * Move Method: uses from method to determin locale index,
+	 * then checks if index is equal to -1, if so tell user they cannot go that way.
+	 * If not, then print out the new location and set the players current location 
+	 * to that location. Then drop a crumb at the current location.
+	 * @param dir: integer that is the cardnial direction user wants to go
+	 * @param command: String represention of cardnial direction.
+	 */
 	public static void move(int dir,String command) {
 		int nextLocation = from(dir);// takes cardinal dir (0,1,2,3) and returns locale index
 	
@@ -240,7 +302,12 @@ public class HannahTextGame {
 		}
 	}
 	
-	// specific command methods
+	// Specific Command Methods
+	
+	/**
+	 * Show Map Method
+	 * This method prints out the map only if the player has the map in invnentory
+	 */
 	public static void showMap () {
 			
 			if (player1.inventory.contains(map1)) {
@@ -260,6 +327,10 @@ public class HannahTextGame {
 		
 		
 	}
+	/**
+	 * Help Method
+	 * This method prints out a list of Commands that the user can enter 
+	 */
 	public static void help () {
 		System.out.println("COMMANDS");
 		System.out.println("--------");
@@ -269,12 +340,24 @@ public class HannahTextGame {
 				          "'X' to examine location for item\n'U' to use item*");
 		System.out.println("*need a specified item after command.\n");
 	}
+	/**
+	 * PLayer Satistics Method
+	 * This Method prints out all of the players current statistics 
+	 * including current location, current points, and inventory.
+	 */
 	public static void playerStats () {
 		
 		System.out.println("Current Location: " + locations[player1.location].name);
 		System.out.println("Current Points: " + player1.score);
 		System.out.println("Inventory: " + player1.inventory);
 	}
+	/**
+	 * Backtrack Method
+	 * This method checks to see if there are any crumbs in trail 
+	 * then it picks up the top crumb, then it checks again to see if there are 
+	 * any more crumbs and if there are it prints out the location of that crumb
+	 * and sets players location to the crumb.
+	 */
 	public static void backtrack () {
 		
 		if (trail.hasMoreCrumbs()) { // checks to see if there is any crumbs left
@@ -291,21 +374,15 @@ public class HannahTextGame {
 		}
 	
 }
-	
-	/** 
-	 * Homemade "to string" method to properly convert 
-	 * user input into string without hashcode. 
-	 * @param userinput
-	 * @return userinput into proper string
+	/**
+	 * Use Method
+	 * Loop to check every spot in inventory list and see if 
+	 * it matches up with the String of the user input. If so,
+	 * Then check to see if it is the limitedUseItem, which if so 
+	 * will see if there are uses left and subtracts one if there is
+	 * then prints out proper message
+	 * @param item: String of item the user wants to use.
 	 */
-	public static String stringConverter (String[] userinput) {
-		StringBuilder sb = new StringBuilder();
-		
-        for(String str : userinput) sb.append(str);
-        return sb.toString().toLowerCase().trim();
-		
-	}
-	
 	public static void use(String item) {	
 		String itemToUse = item;
 		
@@ -330,6 +407,12 @@ public class HannahTextGame {
 		
 		
 	}
+	/**
+	 * Examine Method
+	 * This method checks if there is an item of there or not
+	 * if not then check if the item is discovered and if not it 
+	 * makes it discoverable and prints message. 
+	 */
 	public static void examine() {
 		Item itemAtLocation = locations[player1.location].item;
 		if (itemAtLocation==null) {
